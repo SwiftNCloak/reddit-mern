528a2f1d0aaeefe0448ddbac6c2f25ac1720cc30
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-07-27 21:32:27 +0800

    alright

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,8 +10,8 @@ function App() {
           <img src={Logo} className='w-8 h-8 bg-reddit_dark' />
         </div>
         
-        <form action="" className='bg-reddit_dark-brighter p-1 px-3 flex rounded-md border border-gray-600'>
-          <img src={Search} className="text-gray-300 h-7 w-7"/>
+        <form action="" className='bg-reddit_dark-brighter px-3 flex rounded-md border border-gray-600'>
+          <img src={Search} className="text-gray-300 h-5 w-5 mt-1.5"/>
           <input type="input" className="bg-reddit_dark-brighter text-sm p-1 pl-2 pr-0 block focus:outline-none text-white" placeholder="Search" />
         </form>
       </header>
