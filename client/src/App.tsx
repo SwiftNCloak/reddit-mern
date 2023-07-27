8f76a65f0c0ca022159ff7367de0e00e8520decd
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-07-27 21:18:29 +0800

    modifiedddd lezzgo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import './style.css'
 import Logo from './assets/reddit-logo.png'
+import Search from './assets/search.svg'
 
 function App() {
   return (
@@ -8,8 +9,10 @@ function App() {
         <div className="mx-4">
           <img src={Logo} className='w-8 h-8 bg-reddit_dark' />
         </div>
-        <form action="">
-          <input type="input" className="bg-gray-800 h-8" />
+        
+        <form action="" className='bg-gray-800 p-1 px-2 flex rounded-md'>
+          <img src={Search} className="text-gray-300 h-7 w-7"/>
+          <input type="input" className="bg-gray-800 h-6" />
         </form>
       </header>
     </div>
