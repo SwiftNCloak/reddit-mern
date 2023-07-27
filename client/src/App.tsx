255756c49fd08bca78d027623dd1c8c73217ed99
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-07-27 20:51:37 +0800

    the header bar is on the process

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import './style.css'
+import Logo from './assets/reddit-logo.png'
 
 function App() {
   return (
     <div>
-      <header className='flex'>
-        
+      <header className='flex w-full bg-reddit_dark p-1'>
+        <img src={Logo} className='w-8 h-8 bg-reddit_dark' />
       </header>
     </div>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        reddit_dark: {
+          DEFAULT: '#030303',
+        },
+      },
+    },
   },
   plugins: [],
 }
