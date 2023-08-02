958f296150258c44e31ed95ecfda5a0236f38358
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-08-02 21:24:03 +0800

    modified the logo yey

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import './style.css'
 import Logo from './assets/reddit-logo.png'
-import Search from './assets/search.svg'
+import {MagnifyingGlassIcon} from "@heroicons/react/24/outline";
 
 // The navbar
 function App() {
@@ -12,7 +12,7 @@ function App() {
         </div>
         
         <form action="" className='bg-reddit_dark-brighter px-3 flex rounded-md border border-gray-600'>
-          <img src={Search} className="text-gray-300 h-5 w-5 mt-1.5"/>
+          <MagnifyingGlassIcon className="text-gray-300 h-5 w-5 mt-1.5"/>
           <input type="input" className="bg-reddit_dark-brighter text-sm p-1 pl-2 pr-0 block focus:outline-none text-white" placeholder="Search" />
         </form>
       </header>
