69adc5add6d31661682eef3d9e079c6a588ba7b4
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-08-02 21:38:56 +0800

    turned icons into buttons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,18 +10,18 @@ import {MagnifyingGlassIcon,
 function App() {
   return (
     <div>
-      <header className='flex w-full bg-reddit_dark p-2'>
-        <div className="mx-4">
-          <img src={Logo} className='w-8 h-8 bg-reddit_dark' />
+      <header className='w-full bg-reddit_dark p-2'>
+        <div className="mx-4 flex">
+          <img src={Logo} className='w-8 h-8 bg-reddit_dark mr-4' />
+          <form action="" className='bg-reddit_dark-brighter px-3 flex rounded-md border border-gray-600 mx-4'>
+            <MagnifyingGlassIcon className="text-gray-300 h-5 w-5 mt-1.5"/>
+            <input type="input" className="bg-reddit_dark-brighter text-sm p-1 pl-2 pr-0 block focus:outline-none text-white" placeholder="Search" />
+          </form>
+          
+          <button><ChatBubbleOvalLeftEllipsisIcon className='text-white w-6 h-6 m-1 mx-2'/></button>
+          <button><BellIcon className='text-white w-6 h-6 m-1 mx-2'/></button>
+          <button><PlusIcon className='text-white w-6 h-6 m-1 mx-2'/></button>
         </div>
-        
-        <form action="" className='bg-reddit_dark-brighter px-3 flex rounded-md border border-gray-600'>
-          <MagnifyingGlassIcon className="text-gray-300 h-5 w-5 mt-1.5"/>
-          <input type="input" className="bg-reddit_dark-brighter text-sm p-1 pl-2 pr-0 block focus:outline-none text-white" placeholder="Search" />
-        </form>
-        <ChatBubbleOvalLeftEllipsisIcon className='text-white w-8 h-8'/>
-        <BellIcon className='text-white w-8 h-8'/>
-        <PlusIcon className='text-white w-8 h-8'/>
       </header>
     </div>
   )
