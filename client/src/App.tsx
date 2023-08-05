3a77645cdfb3afffe7dac46c2d98b77462462716
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-08-05 16:52:27 +0800

    yeyeyyee

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import './style.css'
+import Avatar from './assets/reddit-avatar.png'
 
 import Header from './components/Header'
 import BoardHeader from './components/BoardHeader'
@@ -11,9 +12,15 @@ function App() {
       <BoardHeader /> {/* BoardHeader */}
 
       <div className="bg-reddit_dark px-6 py-4 text-gray-400">
-        <div className="border border-reddit_border p-2 rounded-md">
-          
+        <div className="border border-reddit_border p-2 rounded-md flex">
+          <div className="rounded-full bg-gray-600 overflow-hidden w-10 h-10">
+            <img src={Avatar} alt="" style={{filter:'invert(100%)'}}/>
+          </div>
+          <form action="" className='flex-grow'>
+            <input type="text" className='bg-reddit_dark-brighter border border-reddit_border p-2 block rounded-md ml-4' placeholder='New post'/>
+          </form>
         </div>
+        
       </div>
     </div>
   )
