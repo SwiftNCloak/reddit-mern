0eea2c2fa80adc07e9b4a199c5dd5a524f140023
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-08-05 17:01:57 +0800

    changed the size yeah

--- client/src/components/BoardHeader.tsx
+++ client/src/components/BoardHeader.tsx
@@ -3,7 +3,7 @@ import mainimg from '../assets/main.png'
 function BoardHeader(){
     return(
         <>
-            <div className="h-20 bg-cover" style={{backgroundImage:'url(./src/assets/2.png)'}}></div> {/* Subheader */}
+            <div className="h-52 bg-cover" style={{backgroundImage:'url(./src/assets/2.png)'}}></div> {/* Subheader */}
             <div className='bg-reddit_dark-brighter'>
                 <div className="mx-6 relative flex">
                 <div className="h-20 w-20 rounded-full overflow-hidden relative -top-3 border-4 border-white bg-white">
