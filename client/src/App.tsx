5ade8937364492b77700b63410d15d9ff2b1a37b
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-08-05 16:21:08 +0800

    yeyeye

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import './style.css'
+import mainicon from './assets/main.png'
 import Header from './components/Header'
 
 // https://react-icons.github.io/react-icons/icons?name=hi2
@@ -6,10 +7,15 @@ function App() {
   return (
     <div>
       <Header /> {/* Header */}
+      <div className="h-20 bg-cover" style={{backgroundImage:'url(./src/assets/2.png)'}}></div> {/* Subheader */}
       
-      <section className="subheader"> {/* Subheader */}
-
-      </section>
+      <div className='bg-reddit_dark-brighter'>
+        <div className="mx-4">
+          <div className="h-20 w-20">
+            <img src={mainicon} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
