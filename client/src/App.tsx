5b2dfd57177c3f438dfff23a28f1f55ebd3035c9
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-08-11 18:36:20 +0800

    text yey

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <div className='px-6 bg-reddit_dark text-reddit_text'>
         <div className="border border-reddit_border bg-reddit_dark-bright p-2 rounded-md">
           <h5 className="text-reddit_text-dark text-sm mb-1">Posted by u/markyjames 10 hours ago</h5>
-          <h2 className='text-xl mb-3 font-bold'>I miss you na, please? 🥺</h2>
+          <h2 className='text-xl mb-3 font-bold'>Reddit clone, please? 🥺</h2>
           <div className='text-sm leading-6'>
             <p>
               Look at the stars
